ee4424822ae7b3d7352bd067bf7fed9b18a5008f
Enric Cervera <ecervera@uji.es>
2019-09-27 14:37:46 +0200

    add selectable ip

--- scripts/talker.py
+++ scripts/talker.py
@@ -7,16 +7,21 @@ def callback(msg):
    
     global tts
     tts.say(msg.data)
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg.data)
 
     
 def talker():
 
-    global tts
-    tts = ALProxy("ALTextToSpeech", "192.168.1.22", 9559)
-
     rospy.init_node('talker', anonymous=True)
     rospy.Subscriber("talk", String, callback)
+
+    if rospy.has_param('nao_ip'):
+        NAO_IP = rospy.get_param('nao_ip')
+    else:
+        NAO_IP = "127.0.0.1"
+
+    global tts
+    tts = ALProxy("ALTextToSpeech", NAO_IP, 9559)
+
     rospy.spin()
 
 if __name__ == '__main__':
