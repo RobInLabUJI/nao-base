af732a70d6a00accc7b0dcb24da4869962eaf242
Enric Cervera <ecervera@uji.es>
2019-09-29 16:54:55 +0200

    add camera

--- scripts/vision.py
+++ scripts/vision.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import Image
 from naoqi import ALProxy
-    
+import vision_definitions
+
 def vision():
 
     rospy.init_node('vision', anonymous=True)
-    pub = rospy.Publisher('head_joints', Float64MultiArray, queue_size=10)
+    pub = rospy.Publisher('camera/top/camera/image_raw', Image, queue_size=10)
 
     if rospy.has_param('nao_ip'):
         NAO_IP = rospy.get_param('nao_ip')
@@ -16,19 +17,30 @@ def vision():
         NAO_PORT = rospy.get_param('nao_port')
     else:
         NAO_PORT = 9559
+    cameraID = 0
 
-    global motionProxy
-    motionProxy = ALProxy("ALMotion", NAO_IP, NAO_PORT)
-    motionProxy.setStiffnesses("Head", 1.0)
+    videoProxy = ALProxy("ALVideoDevice", NAO_IP, NAO_PORT)
+    resolution = vision_definitions.kQVGA  # 320 * 240
+    colorSpace = vision_definitions.kRGBColorSpace
+    imgClient = videoProxy.subscribe("client", resolution, colorSpace, 5)
+    videoProxy.setParam(vision_definitions.kCameraSelectID, cameraID)
 
     r = rospy.Rate(10) # 10hz
-    names = "Head"
-    useSensors  = True
     while not rospy.is_shutdown():
-        sensorAngles = motionProxy.getAngles(names, useSensors)
-        msg = Float64MultiArray(data=sensorAngles)
-        pub.publish(msg)
+        alImage = videoProxy.getImageRemote(imgClient)
+        imageWidth  = alImage[0]
+        imageHeight = alImage[1]
+        imageArray  = alImage[6]
+        im = Image()
+        im.height = imageHeight
+        im.width  = imageWidth
+        im.encoding = "rgb8"
+        im.is_bigendian = 0
+        im.step = 960
+        im.data = imageArray
+        pub.publish(im)
         r.sleep()
+    videoProxy.unsubscribe(imgClient)
 
 if __name__ == '__main__':
     vision()
