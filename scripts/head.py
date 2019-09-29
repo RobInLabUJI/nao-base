ec7f8480a898ba968bf692e5ecf36d97710b8be4
Enric Cervera <ecervera@uji.es>
2019-09-29 13:16:47 +0200

    publish head joints

--- scripts/head.py
+++ scripts/head.py
@@ -6,16 +6,16 @@ from naoqi import ALProxy
 def callback(msg):
    
     global motionProxy
-    #tts.say(msg.data)
     names            = ["HeadYaw", "HeadPitch"]
     angles           = msg.data[0:2]
     fractionMaxSpeed = 0.1
-    motionProxy.setAngles(names,angles,fractionMaxSpeed)
+    motionProxy.setAngles(names, angles, fractionMaxSpeed)
     
 def head():
 
     rospy.init_node('head', anonymous=True)
     rospy.Subscriber("head_angles", Float64MultiArray, callback)
+    pub = rospy.Publisher('head_joints', Float64MultiArray, queue_size=10)
 
     if rospy.has_param('nao_ip'):
         NAO_IP = rospy.get_param('nao_ip')
@@ -29,7 +29,15 @@ def head():
     global motionProxy
     motionProxy = ALProxy("ALMotion", NAO_IP, NAO_PORT)
     motionProxy.setStiffnesses("Head", 1.0)
-    rospy.spin()
+
+    r = rospy.Rate(10) # 10hz
+    names = "Head"
+    useSensors  = True
+    while not rospy.is_shutdown():
+        sensorAngles = motionProxy.getAngles(names, useSensors)
+        msg = Float64MultiArray(data=sensorAngles)
+        pub.publish(msg)
+        r.sleep()
 
 if __name__ == '__main__':
     head()
