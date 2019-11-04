b98d2a986991a4af463f3c058045fa092504a979
Enric Cervera <ecervera@uji.es>
2019-11-04 10:15:55 +0100

    publish recognized word

--- scripts/speech.py
+++ scripts/speech.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 import rospy
+import time
 from std_msgs.msg import String
 from naoqi import ALProxy
 
 def callback(msg):
    
     global tts
+    global asr
+    asr.pause(True)
     tts.say(msg.data)
-
+    asr.pause(False)
     
 def speech():
 
     rospy.init_node('speech', anonymous=True)
     rospy.Subscriber('speech', String, callback)
-    pub = rospy.Publisher('word_recognized', Float64MultiArray, queue_size=10)
+    pub = rospy.Publisher('word_recognized', String, queue_size=10)
 
     if rospy.has_param('nao_ip'):
         NAO_IP = rospy.get_param('nao_ip')
@@ -26,17 +29,33 @@ def speech():
         NAO_PORT = 9559
 
     global tts
+    global asr
     tts = ALProxy('ALTextToSpeech', NAO_IP, NAO_PORT)
     asr = ALProxy("ALSpeechRecognition", NAO_IP, NAO_PORT)
+    #asr.unsubscribe("Test_ASR")
 
     asr.setLanguage("English")
-    vocabulary = ["yes", "no", "please"]
+    vocabulary = ["yes", "no", "one", "two", "three"]
     asr.setVocabulary(vocabulary, False)
     asr.subscribe("Test_ASR")
 
-    r = rospy.Rate(10) # 10hz
+    memValueSpeech = "SpeechDetected"
+    memValueWord   = "WordRecognized"
+    try:
+      memoryProxy = ALProxy("ALMemory", NAO_IP, NAO_PORT)
+    except Exception, e:
+      print "Error when creating memory proxy:"
+      print str(e)
+      exit(1)
+
+    r = rospy.Rate(2) # 2hz
     while not rospy.is_shutdown():
-        #pub.publish(msg)
+        speechDetected = memoryProxy.getData(memValueSpeech)
+        if speechDetected:
+            time.sleep(1.0)
+            wordRecognized = memoryProxy.getData(memValueWord)
+            print(wordRecognized)
+            pub.publish(wordRecognized[0])
         r.sleep()
     asr.unsubscribe("Test_ASR")
 
